006e50f673f10d7de035df5bf814aa71be1f7a5a
Fabio Saito <fabio.h.saito@gmail.com>
2022-04-06 13:53:01 -0300

    specify http response to only parse body

--- app/services/status_invest_scraper.rb
+++ app/services/status_invest_scraper.rb
@@ -4,7 +4,7 @@ class StatusInvestScraper
   def self.call(ticker)
     url = "https://statusinvest.com.br/acoes/#{ticker}"
     unparsed_page = HTTParty.get(url)
-    parsed_page = Nokogiri::HTML(unparsed_page)
+    parsed_page = Nokogiri::HTML(unparsed_page.body)
     quote_value = parsed_page.css('strong.value').first.text.gsub(',', '').to_i
   end
 end
