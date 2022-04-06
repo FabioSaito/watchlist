041e8120ba9806f3837aa175ae4b28ad29425680
Fabio Saito <fabio.h.saito@gmail.com>
2022-04-06 13:57:55 -0300

    remove whitespace

--- spec/sidekiq/update_stock_quote_job_spec.rb
+++ spec/sidekiq/update_stock_quote_job_spec.rb
@@ -2,11 +2,12 @@ require 'rails_helper'
 
 RSpec.describe UpdateStockQuoteJob, type: :job do
   context 'when UpdateStockQuoteJob is performed' do
-    
+
+  end
     it 'increases queue by 1' do
       expect { UpdateStockQuoteJob.perform_async('PETR4') }.to change(UpdateStockQuoteJob.jobs, :size).by(1)
     end
-    
+
     it 'add asset to queue' do
       UpdateStockQuoteJob.perform_async('PETR4')
       expect(UpdateStockQuoteJob).to have_enqueued_sidekiq_job('PETR4')
