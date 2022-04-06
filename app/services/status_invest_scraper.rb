86723d329a2e2f45406219f56a1be815c1206486
Fabio Saito <fabio.h.saito@gmail.com>
2022-04-06 10:32:51 -0300

    create service to webscraper

---
+++ app/services/status_invest_scraper.rb
@@ -0,0 +1,10 @@
+class StatusInvestScraper
+  private_class_method :new
+
+  def self.call(ticker)
+    url = "https://statusinvest.com.br/acoes/#{ticker}"
+    unparsed_page = HTTParty.get(url)
+    parsed_page = Nokogiri::HTML(unparsed_page)
+    quote_value = parsed_page.css('strong.value').first.text.gsub(',', '').to_i
+  end
+end
