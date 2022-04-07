39c959084eb6d00613f6907494bc4dfab446d977
Fabio Saito <fabio.h.saito@gmail.com>
2022-04-07 13:13:26 -0300

    add priority to queue

--- app/services/update_quotes.rb
+++ app/services/update_quotes.rb
@@ -2,8 +2,10 @@ class UpdateQuotes
   private_class_method :new
 
   def self.call
+    watchlists = WalletItem.all.pluck(:asset_id)
     Asset.where(currency: 'BRL').each do |asset|
-      UpdateStockQuoteJob.perform_async(asset.symbol)
+      queue_type = watchlists.include?(asset.id) ? :critical : :default
+      UpdateStockQuoteJob.set(queue: queue_type).perform_async(asset.symbol)
     end
   end
 end
