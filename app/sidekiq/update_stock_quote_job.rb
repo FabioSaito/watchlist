e15d2295b8cb7853b159eb3365b92e2c8e49edc6
Fabio Saito <fabio.h.saito@gmail.com>
2022-04-11 15:40:26 -0300

    add message to sidekiq max retries

--- app/sidekiq/update_stock_quote_job.rb
+++ app/sidekiq/update_stock_quote_job.rb
@@ -1,5 +1,10 @@
 class UpdateStockQuoteJob
   include Sidekiq::Job
+  sidekiq_options retry: 5
+
+  sidekiq_retries_exhausted do |msg|
+    Alert.create!(message: "[ERROR] Ticker: #{msg['args'][0]} could not be fetched!")
+  end
 
   def perform(params)
     QuoteCreator.call(params)
