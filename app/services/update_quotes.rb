e0d17bd9d6d65025fcbb116b0dc2f35bd9038f25
Fabio Saito <fabio.h.saito@gmail.com>
2022-04-18 12:14:01 -0300

    refactor update quotes

--- app/services/update_quotes.rb
+++ app/services/update_quotes.rb
@@ -1,10 +1,22 @@
 class UpdateQuotes
   private_class_method :new
 
-  def self.call
-    watchlists = WalletItem.all.pluck(:asset_id)
-    Asset.where(currency: 'BRL').each do |asset|
-      watchlists.include?(asset.id) ? UpdateStockQuoteJob.high_priority(asset.symbol) : UpdateStockQuoteJob.low_priority(asset.symbol)
+  class << self
+    def call
+      watchlist = WalletItem.pluck(:asset_id)
+
+      Asset.where(currency: 'BRL').each do |asset|
+        UpdateStockQuoteJob.send(
+          priority_by_asset(watchlist, asset.id),
+          asset.symbol
+        )
+      end
+    end
+
+    private
+
+    def priority_by_asset(watchlist, asset_id)
+      watchlist.include?(asset_id) ? :high_priority : :low_priority
     end
   end
 end
